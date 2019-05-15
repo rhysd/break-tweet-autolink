fb348a503d51df950236784ce03720c63de2f99a
rhysd <lin90162@yahoo.co.jp>
2019-05-16 01:44:40 +0900

    fix keys of saved options by type checking

--- pkg/unlink-tweet-chrome/options.ts
+++ pkg/unlink-tweet-chrome/options.ts
@@ -1,27 +1,29 @@
+import { TweetAutoLinkBreakerConfigAll as ConfigAll } from 'break-tweet-autolink';
+
 // prettier-ignore
 const OPTION_NAMES = [
     'mention',
     'list',
     'hashtag',
     'cashtag',
-    'url-no-scheme',
-    'url-with-scheme',
+    'urlNoScheme',
+    'urlWithScheme',
 ] as const;
 
+// Note: Don't use `keyof ConfigAll` to check OPTION_NAMES has proper values
 type OptionName = typeof OPTION_NAMES[number];
-type Options = { [K in OptionName]: boolean };
 
 const saveButton = document.getElementById('save-btn')! as HTMLButtonElement;
 const resetButton = document.getElementById('reset-btn')! as HTMLButtonElement;
 
 // prettier-ignore
-const DEFAULT_OPTIONS: Options = {
+const DEFAULT_OPTIONS: ConfigAll = {
     'mention': true,
     'list': true,
     'hashtag': true,
     'cashtag': true,
-    'url-no-scheme': true,
-    'url-with-scheme': false,
+    'urlNoScheme': true,
+    'urlWithScheme': false,
 };
 
 function optionElement(name: OptionName): HTMLInputElement {
@@ -32,7 +34,7 @@ function optionElement(name: OptionName): HTMLInputElement {
     return elem as HTMLInputElement;
 }
 
-function readOptionsFromElems(): Options {
+function readOptionsFromElems(): ConfigAll {
     // Note: es2019.object.d.ts does not work with string literal types as keys of object.
     // The return type of Object.fromEntries() falls back to {[k: string]: T; [k: number]: T}.
     const ret = {} as any;
@@ -42,20 +44,20 @@ function readOptionsFromElems(): Options {
     return ret;
 }
 
-function setOptionsToElems(opts: Options) {
+function setOptionsToElems(opts: ConfigAll) {
     for (const k of Object.keys(opts)) {
         optionElement(k as OptionName).checked = opts[k as OptionName];
     }
 }
 
-function saveOptionsToStorage(opts: Options) {
+function saveOptionsToStorage(opts: ConfigAll) {
     return new Promise<void>(resolve => chrome.storage.sync.set(opts, resolve));
 }
 
 // Note: The loaded object may be empty when the value is not saved yet
 function loadOptionsFromStrage() {
-    return new Promise<Options>(resolve => {
-        chrome.storage.sync.get(OPTION_NAMES, (opts: Options) => resolve(opts));
+    return new Promise<ConfigAll>(resolve => {
+        chrome.storage.sync.get(OPTION_NAMES, (opts: ConfigAll) => resolve(opts));
     });
 }
 
